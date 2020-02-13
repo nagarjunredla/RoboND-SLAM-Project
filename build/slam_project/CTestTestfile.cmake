474318a6b1b69f6dba86101800b90e8a5ffe5ca0
Nagarjun Redla <nagarjunredla@gmail.com>
2020-02-13 03:39:35 -0800

    updated for new rubric

--- build/slam_project/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/map_my_world_ws/src/slam_project
-# Build directory: /home/workspace/map_my_world_ws/build/slam_project
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/udacity_bot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/map_my_world_ws/src/udacity_bot
-# Build directory: /home/workspace/map_my_world_ws/build/udacity_bot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
